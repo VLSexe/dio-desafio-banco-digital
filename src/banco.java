4c6012cc051c3d0e2ca9990ca3b123e78ecf0426
Victor Lopes dos Santos <106280251+VLSexe@users.noreply.github.com>
2024-08-14 20:49:27 -0300

    Update banco.java

--- src/banco.java
+++ src/banco.java
@@ -1,25 +1,19 @@
 import java.util.List;
 
 public class banco {
-	
 	private String nome;
 	private List<conta> contas;
 
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public List<conta> getContas() {
 		return contas;
 	}
-
 	public void setContas(List<conta> contas) {
 		this.contas = contas;
 	}
-	
-	
 }
