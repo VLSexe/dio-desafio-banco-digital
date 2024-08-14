ff7dd17c8c02be201aa9d46760434c84714830a1
Victor Lopes dos Santos <106280251+VLSexe@users.noreply.github.com>
2024-08-14 20:48:38 -0300

    Update contaPoupanca.java

--- src/contaPoupanca.java
+++ src/contaPoupanca.java
@@ -1,15 +1,12 @@
 
 public class contaPoupanca extends conta {	
-	
 	public contaPoupanca(client client) {
 		super(client);
-		
 	}
+	
 	@Override
 	public void imprimirExtrato() {
 		System.out.println("=== Extrato conta Poupanca ===");
 		super.imprimirInfosComuns();
-	
 	}
-		
-	}
+}
