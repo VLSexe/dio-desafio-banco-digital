c7d7c33258222dac10adb807ecdd900597090e3d
Victor Lopes dos Santos <106280251+VLSexe@users.noreply.github.com>
2024-08-14 20:48:05 -0300

    Update iConta.java

--- src/iConta.java
+++ src/iConta.java
@@ -1,18 +1,7 @@
 
 public interface iConta {
-	
 	void sacar(double valor);
-	
 	void depositar(double valor);
-	
 	void transferir(double valor, iConta contaDestino);
-	
 	void imprimirExtrato();
-
-
-	
-
-
-
-
 }
